4e80299d35c9786be4d22fe2c1ec94bd5dbaa455
tah182 <tah.tatsumoto@gmail.com>
2022-04-02 01:28:35 -0600

    Time: 170 ms (15.16%), Space: 41.2 MB (5.08%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        var finished = false;
+        var index = 0;
+        var longestPrefix = "";
+        
+        while (!finished) {
+            char? temp = null;
+    
+            if (strs[0].Length == 0)
+                return longestPrefix;
+            else if (strs[0].Length > index)
+                temp = strs[0][index];
+            
+            foreach(var word in strs) {
+                Console.WriteLine($"Checking word: [{word}]");
+                if (index >= word.Length || word[index] != temp)
+                {
+                    finished = true;
+                    break;
+                }
+            }
+            if (!finished)
+                longestPrefix += temp;
+            
+            Console.WriteLine($"LP: [{longestPrefix}] -- index: [{index}]");
+            index++;
+        }
+        
+        Console.WriteLine($"Longest prefix = {longestPrefix}");
+        return longestPrefix;
+    }
+}
