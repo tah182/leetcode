b7685ac64d2b4cc126b5f4f0817f8730da7fece5
tah182 <tah.tatsumoto@gmail.com>
2022-04-02 13:29:04 -0600

    Time: 229 ms (5.10%), Space: 40.1 MB (18.42%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cs
+++ 13-roman-to-integer/13-roman-to-integer.cs
@@ -1,58 +1,90 @@
 public class Solution {
-    private List<Roman> romans = new List<Roman> {
-        new Roman('I', 1),
-        new Roman('V', 5),
-        new Roman('X', 10),
-        new Roman('L', 50),
-        new Roman('C', 100),
-        new Roman('D', 500),
-        new Roman('M', 1000)
-    };
-    
     public int RomanToInt(string s) {
-        var total = 0;
         
+        var total = 0;
         for (int i = 0; i < s.Length; i++) {
-            foreach (var roman in romans) {
-                if (roman.Letter == s[i]) {
-                    var nextLetter = i != s.Length - 1 ? s[i+1] : '?';
-                    var numerical = roman.getNumber(nextLetter);
-                    if (numerical != roman.Number)
-                        i++;
-                    total += numerical;
-                    
+            Console.WriteLine($"i == {i} of {s}");
+            switch (s[i]) {
+                case 'M':
+                    total += 1000;
+                    Console.WriteLine($"added: {s[i]} total [{total}]");
                     break;
-                }
-            }
-            
+                case 'D':
+                    total += 500;
+                    Console.WriteLine($"added: {s[i]} total [{total}]");
+                    break;
+                case 'C':
+                    if (i < s.Length - 1) {
+                        if (s[i + 1] == 'D')
+                        {
+                            total += 400;
+                            Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
+                            i++;
+                            continue;
+                        }
+                        if (s[i + 1] == 'M')
+                        {
+                            total += 900;
+                            Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
+                            i++;
+                            continue;
+                        }
+                    }
+                    Console.WriteLine($"added: {s[i]} total [{total}]");
+                    total += 100;
+                    break;
+                case 'L':
+                    total += 50;
+                    Console.WriteLine($"added: {s[i]} total [{total}]");
+                    break;
+                case 'X':
+                    if (i < s.Length - 1) {
+                        if (s[i + 1] == 'L')
+                        {
+                            total += 40;
+                            Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
+                            i++;
+                            continue;
+                        }
+                        if (s[i + 1] == 'C')
+                        {
+                            total += 90;
+                            Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
+                            i++;
+                            continue;
+                        }
+                    }
+                    Console.WriteLine($"added: {s[i]} total [{total}]");
+                    total += 10;
+                    break;
+                case 'V':
+                    total += 5;
+                    Console.WriteLine($"added: {s[i]} total [{total}]");
+                    break;
+                default:
+                    if (i < s.Length - 1) {
+                        if (s[i + 1] == 'V')
+                        {
+                            total += 4;
+                            Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
+                            i++;
+                            continue;
+                        }
+                        if (s[i + 1] == 'X')
+                        {
+                            total += 9;
+                            Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
+                            i++;
+                            continue;
+                        }
+                    }
+                    Console.WriteLine($"added: {s[i]} total [{total}]");
+                    total += 1;
+                    break;
+            }   
         }
         
         return total;
     }
     
-    class Roman {
-        public Roman (char l, int n) {
-            this.Letter = l;
-            this.Number = n;
-        }
-        
-        public char Letter { get; private set; }
-        public int Number { get; private set; }
-        public int getNumber (char nextLetter) {
-            if (this.Letter == 'I' && nextLetter == 'V')
-                return 4;
-            if (this.Letter == 'I' && nextLetter == 'X')
-                return 9;
-            if (this.Letter == 'X' && nextLetter == 'L')
-                return 40;
-            if (this.Letter == 'X' && nextLetter == 'C')
-                return 90;
-            if (this.Letter == 'C' && nextLetter == 'D')
-                return 400;
-            if (this.Letter == 'C' && nextLetter == 'M')
-                return 900;
-            
-            return this.Number;
-        }
-    }
 }
