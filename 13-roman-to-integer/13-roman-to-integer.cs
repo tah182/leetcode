8ee1bc9215fedce62e87d6c0b4cae979f96c794a
tah182 <tah.tatsumoto@gmail.com>
2022-04-02 13:29:57 -0600

    Time: 176 ms (5.10%), Space: 37.2 MB (25.31%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cs
+++ 13-roman-to-integer/13-roman-to-integer.cs
@@ -7,78 +7,78 @@ public class Solution {
             switch (s[i]) {
                 case 'M':
                     total += 1000;
-                    Console.WriteLine($"added: {s[i]} total [{total}]");
+                    //Console.WriteLine($"added: {s[i]} total [{total}]");
                     break;
                 case 'D':
                     total += 500;
-                    Console.WriteLine($"added: {s[i]} total [{total}]");
+                    //Console.WriteLine($"added: {s[i]} total [{total}]");
                     break;
                 case 'C':
                     if (i < s.Length - 1) {
                         if (s[i + 1] == 'D')
                         {
                             total += 400;
-                            Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
+                            //Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
                             i++;
                             continue;
                         }
                         if (s[i + 1] == 'M')
                         {
                             total += 900;
-                            Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
+                            //Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
                             i++;
                             continue;
                         }
                     }
-                    Console.WriteLine($"added: {s[i]} total [{total}]");
+                    //Console.WriteLine($"added: {s[i]} total [{total}]");
                     total += 100;
                     break;
                 case 'L':
                     total += 50;
-                    Console.WriteLine($"added: {s[i]} total [{total}]");
+                    //Console.WriteLine($"added: {s[i]} total [{total}]");
                     break;
                 case 'X':
                     if (i < s.Length - 1) {
                         if (s[i + 1] == 'L')
                         {
                             total += 40;
-                            Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
+                            //Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
                             i++;
                             continue;
                         }
                         if (s[i + 1] == 'C')
                         {
                             total += 90;
-                            Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
+                            //Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
                             i++;
                             continue;
                         }
                     }
-                    Console.WriteLine($"added: {s[i]} total [{total}]");
+                    //Console.WriteLine($"added: {s[i]} total [{total}]");
                     total += 10;
                     break;
                 case 'V':
                     total += 5;
-                    Console.WriteLine($"added: {s[i]} total [{total}]");
+                    //Console.WriteLine($"added: {s[i]} total [{total}]");
                     break;
                 default:
                     if (i < s.Length - 1) {
                         if (s[i + 1] == 'V')
                         {
                             total += 4;
-                            Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
+                            //Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
                             i++;
                             continue;
                         }
                         if (s[i + 1] == 'X')
                         {
                             total += 9;
-                            Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
+                            //Console.WriteLine($"added: {s[i]}{s[i + 1]} total [{total}]");
                             i++;
                             continue;
                         }
                     }
-                    Console.WriteLine($"added: {s[i]} total [{total}]");
+                    //Console.WriteLine($"added: {s[i]} total [{total}]");
                     total += 1;
                     break;
             }   
