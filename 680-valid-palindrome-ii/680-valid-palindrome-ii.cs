64c5369410a798d383609b28994009d123c6b2c7
tah182 <tah.tatsumoto@gmail.com>
2022-04-02 13:45:51 -0600

    Time: 121 ms (59.24%), Space: 43.5 MB (18.58%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public bool ValidPalindrome(string s) {
+        return isPalindrome(s, 0, s.Length - 1, false);
+    }
+    
+    bool isPalindrome(string s, int left, int right, bool skippedOnce) {
+        // special cases
+        if (left >= right)
+            return true;
+        
+        //Console.WriteLine($"{s} checking {s[left]} v {s[right]} -- skippedOnce: {skippedOnce}");
+        
+        if (s[left] == s[right])
+            return isPalindrome(s, left + 1, right - 1, skippedOnce);
+        
+        if (!skippedOnce)
+        {
+            if (s[left] == s[right - 1] && s[left + 1] == s[right])
+                return isPalindrome(s, left + 1, right - 2, true) || isPalindrome(s, left + 2, right - 1, true);
+            if (s[left] == s[right - 1])
+                return isPalindrome(s, left + 1, right - 2, true);
+            if (s[left + 1] == s[right])
+                return isPalindrome(s, left + 2, right - 1, true);
+        }
+        
+        return false;
+    }
+}
