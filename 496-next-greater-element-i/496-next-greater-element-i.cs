4eaf526b7120b121025b164136f4ada93ae38a20
tah182 <tah.tatsumoto@gmail.com>
2022-04-03 13:55:40 -0600

    Time: 170 ms (70.70%), Space: 41.7 MB (89.56%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cs
+++ 496-next-greater-element-i/496-next-greater-element-i.cs
@@ -1,25 +1,27 @@
 public class Solution {
-    Dictionary<int, int> indexes;
+    // Dictionary<int, int> indexes;
     public int[] NextGreaterElement(int[] nums1, int[] nums2) {
-        indexes = new Dictionary<int, int>();
+        // indexes = new Dictionary<int, int>();
         
-        for(int i = 0; i < nums2.Length; i++)
-            indexes.Add(nums2[i], i);
+        // for(int i = 0; i < nums2.Length; i++)
+        //     indexes.Add(nums2[i], i);
         
         for (int i = 0; i < nums1.Length; i++) {
-            nums1[i] = indexes.Where(c => c.Key > nums1[i] && c.Value > indexes[nums1[i]])
-                   .Select(c => (KeyValuePair<int, int>?)c)
-                   .FirstOrDefault()?.Key ?? -1;
+            // nums1[i] = indexes.Where(c => c.Key > nums1[i] && c.Value > indexes[nums1[i]])
+            //        .Select(c => (KeyValuePair<int, int>?)c)
+            //        .FirstOrDefault()?.Key ?? -1;
             // var startIndex = indexes[nums1[i]];
-            // var done = false;
-            // for (int j = startIndex; j < nums2.Length && !done; j++) {
-            //     if (nums2[j] > nums1[i]) {
-            //         done = true;
-            //         nums1[i] = nums2[j];
-            //     }
-            // }
-            // if (!done)
-            //     nums1[i] = -1;
+            var done = false;
+            var found = false;
+            for (int j = 0; j < nums2.Length && !done; j++) {
+                if (found && nums2[j] > nums1[i]) {
+                    done = true;
+                    nums1[i] = nums2[j];
+                } if (nums2[j] == nums1[i])
+                    found = true;
+            }
+            if (!done)
+                nums1[i] = -1;
         }
         return nums1;
     }
