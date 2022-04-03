c8c541962c1434b5b9b755aa476910ce6e6dc158
tah182 <tah.tatsumoto@gmail.com>
2022-04-02 22:00:32 -0600

    Time: 104 ms (70.36%), Space: 37.8 MB (59.59%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cs
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cs
@@ -1,12 +1,12 @@
 public class Solution {
     public int ArraySign(int[] nums) {
-        var signFunc = 1;
+        var signFunc = true;
         foreach(var num in nums) {
             if (num == 0)
                 return 0;
             if (num < 0)
-                signFunc *= -1;
+                signFunc = signFunc == false;
         }
-        return signFunc;
+        return signFunc ? 1 : -1;
     }
 }
