ea602b8143742feae71b5aa166f2c551d0f9900b
tah182 <tah.tatsumoto@gmail.com>
2022-04-03 13:43:20 -0600

    Time: 136 ms (86.26%), Space: 43.9 MB (5.31%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cs
+++ 496-next-greater-element-i/496-next-greater-element-i.cs
@@ -1,14 +1,19 @@
 public class Solution {
+    Dictionary<int, int> indexes;
     public int[] NextGreaterElement(int[] nums1, int[] nums2) {
+        indexes = new Dictionary<int, int>();
+        
+        for(int i = 0; i < nums2.Length; i++)
+            indexes.Add(nums2[i], i);
+        
         for (int i = 0; i < nums1.Length; i++) {
-            var found = false;
+            var startIndex = indexes[nums1[i]];
             var done = false;
-            for (int j = 0; j < nums2.Length && !done; j++) {
-                if (found && nums2[j] > nums1[i]) {
+            for (int j = startIndex; j < nums2.Length && !done; j++) {
+                if (nums2[j] > nums1[i]) {
                     done = true;
                     nums1[i] = nums2[j];
-                } else if (nums2[j] == nums1[i])
-                    found = true;
+                }
             }
             if (!done)
                 nums1[i] = -1;
