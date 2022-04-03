01503eff634dd3fdc1a6e73d5c9fbbbe61fc2041
tah182 <tah.tatsumoto@gmail.com>
2022-04-03 13:48:48 -0600

    Time: 347 ms (5.12%), Space: 44.1 MB (5.31%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cs
+++ 496-next-greater-element-i/496-next-greater-element-i.cs
@@ -7,16 +7,21 @@ public class Solution {
             indexes.Add(nums2[i], i);
         
         for (int i = 0; i < nums1.Length; i++) {
-            var startIndex = indexes[nums1[i]];
-            var done = false;
-            for (int j = startIndex; j < nums2.Length && !done; j++) {
-                if (nums2[j] > nums1[i]) {
-                    done = true;
-                    nums1[i] = nums2[j];
-                }
-            }
-            if (!done)
-                nums1[i] = -1;
+            var index = indexes[nums1[i]];
+            nums1[i] = indexes.Where(c => c.Key > nums1[i] && c.Value > index)
+                   .OrderBy(c => c.Value)
+                   .Select(c => (KeyValuePair<int, int>?)c)
+                   .FirstOrDefault()?.Key ?? -1;
+            // var startIndex = indexes[nums1[i]];
+            // var done = false;
+            // for (int j = startIndex; j < nums2.Length && !done; j++) {
+            //     if (nums2[j] > nums1[i]) {
+            //         done = true;
+            //         nums1[i] = nums2[j];
+            //     }
+            // }
+            // if (!done)
+            //     nums1[i] = -1;
         }
         return nums1;
     }
