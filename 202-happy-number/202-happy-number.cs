9ff751dea34fa9e6f71f659681954ba57262c5a6
tah182 <tah.tatsumoto@gmail.com>
2022-04-02 22:35:44 -0600

    Time: 59 ms (18.41%), Space: 29.3 MB (11.90%) - LeetHub

--- 202-happy-number/202-happy-number.cs
+++ 202-happy-number/202-happy-number.cs
@@ -2,9 +2,8 @@ public class Solution {
     Dictionary<int, List<int>> prevNum = new Dictionary<int, List<int>>();
     
     public bool IsHappy(int n) {
-        var numArray = n.ToString().ToCharArray().Select(c => c - '0');
         //Console.WriteLine($"numberArray [{string.Join(",", numArray)}]");
-        var nextNum = numArray.Aggregate(0, (curr, val) => curr + val * val);
+        var nextNum = n.ToString().ToCharArray().Select(c => c - '0').Aggregate(0, (curr, val) => curr + val * val);
         //Console.WriteLine($"sumSquare = {nextNum}");
         
         if (nextNum == 1)
