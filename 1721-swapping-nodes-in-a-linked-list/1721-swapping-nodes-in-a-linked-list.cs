562a70eff59f53bf4b7d484723019f1a53547e37
tah182 <tah.tatsumoto@gmail.com>
2022-04-03 22:23:12 -0600

    Time: 419 ms (34.80%), Space: 52 MB (13.23%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cs
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cs
@@ -20,34 +20,6 @@ public class Solution {
         nodes[k - 1].val = nodes[nodes.Count - k].val;
         nodes[nodes.Count - k].val = temp;
         
-//         var kthStart = KthFromBeginning(head, 1, k);
-//         var kthEnd = KthFromEnd(head, k);
-//         //Console.WriteLine($"k[{k}]th from beginning Val = {kthStart.val}");
-//         //Console.WriteLine($"k[{k}]th from end Val = {kthEnd.val}");
-        
-//         var temp = kthStart.val;
-//         kthStart.val = kthEnd.val;
-//         kthEnd.val = temp;
-        
         return head;
     }
-    
-//     public ListNode KthFromBeginning(ListNode current, int currentDepth, int k) {
-//         if (currentDepth == k)
-//             return current;
-        
-//         return KthFromBeginning(current.next, ++currentDepth, k);
-//     }
-    
-//     public ListNode KthFromEnd(ListNode current, int k) {
-//         List<ListNode> nodes = new List<ListNode>();
-//         while (current.next != null) {
-//             nodes.Add(current);
-//             current = current.next;
-//         }
-//         nodes.Add(current);
-        
-//         //Console.WriteLine($"fullnode list: [{string.Join(",", nodes.Select(n => n.val))}]");
-//         return nodes[nodes.Count - k];
-//     }
 }
