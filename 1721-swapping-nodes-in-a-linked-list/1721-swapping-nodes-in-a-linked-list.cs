27e6dbd8f83ec88c5c6c37431ae0150f8e4b2509
tah182 <tah.tatsumoto@gmail.com>
2022-04-03 22:13:58 -0600

    Time: 430 ms (32.35%), Space: 51.9 MB (15.20%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cs
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cs
@@ -11,26 +11,7 @@
  */
 public class Solution {
     public ListNode SwapNodes(ListNode head, int k) {
-        var kthStart = KthFromBeginning(head, 1, k);
-        var kthEnd = KthFromEnd(head, k);
-        //Console.WriteLine($"k[{k}]th from beginning Val = {kthStart.val}");
-        //Console.WriteLine($"k[{k}]th from end Val = {kthEnd.val}");
-        
-        var temp = kthStart.val;
-        kthStart.val = kthEnd.val;
-        kthEnd.val = temp;
-        
-        return head;
-    }
-    
-    public ListNode KthFromBeginning(ListNode current, int currentDepth, int k) {
-        if (currentDepth == k)
-            return current;
-        
-        return KthFromBeginning(current.next, ++currentDepth, k);
-    }
-    
-    public ListNode KthFromEnd(ListNode current, int k) {
+        var current = head;
         List<ListNode> nodes = new List<ListNode>();
         while (current.next != null) {
             nodes.Add(current);
@@ -38,7 +19,38 @@ public class Solution {
         }
         nodes.Add(current);
         
-        //Console.WriteLine($"fullnode list: [{string.Join(",", nodes.Select(n => n.val))}]");
-        return nodes[nodes.Count - k];
+        var temp = nodes[k - 1].val;
+        nodes[k - 1].val = nodes[nodes.Count - k].val;
+        nodes[nodes.Count - k].val = temp;
+        
+//         var kthStart = KthFromBeginning(head, 1, k);
+//         var kthEnd = KthFromEnd(head, k);
+//         //Console.WriteLine($"k[{k}]th from beginning Val = {kthStart.val}");
+//         //Console.WriteLine($"k[{k}]th from end Val = {kthEnd.val}");
+        
+//         var temp = kthStart.val;
+//         kthStart.val = kthEnd.val;
+//         kthEnd.val = temp;
+        
+        return head;
     }
+    
+//     public ListNode KthFromBeginning(ListNode current, int currentDepth, int k) {
+//         if (currentDepth == k)
+//             return current;
+        
+//         return KthFromBeginning(current.next, ++currentDepth, k);
+//     }
+    
+//     public ListNode KthFromEnd(ListNode current, int k) {
+//         List<ListNode> nodes = new List<ListNode>();
+//         while (current.next != null) {
+//             nodes.Add(current);
+//             current = current.next;
+//         }
+//         nodes.Add(current);
+        
+//         //Console.WriteLine($"fullnode list: [{string.Join(",", nodes.Select(n => n.val))}]");
+//         return nodes[nodes.Count - k];
+//     }
 }
