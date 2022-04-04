806b319443b213dc81802cedf8167942069cd692
tah182 <tah.tatsumoto@gmail.com>
2022-04-03 22:07:44 -0600

    Time: 493 ms (16.67%), Space: 54.7 MB (5.39%) - LeetHub

---
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cs
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode SwapNodes(ListNode head, int k) {
+        var kthStart = KthFromBeginning(head, 1, k);
+        var kthEnd = KthFromEnd(head, k);
+        //Console.WriteLine($"k[{k}]th from beginning Val = {kthStart.val}");
+        //Console.WriteLine($"k[{k}]th from end Val = {kthEnd.val}");
+        
+        var temp = kthStart.val;
+        kthStart.val = kthEnd.val;
+        kthEnd.val = temp;
+        
+        return head;
+    }
+    
+    public ListNode KthFromBeginning(ListNode current, int currentDepth, int k) {
+        if (currentDepth == k)
+            return current;
+        
+        return KthFromBeginning(current.next, ++currentDepth, k);
+    }
+    
+    public ListNode KthFromEnd(ListNode current, int k) {
+        List<ListNode> nodes = new List<ListNode>();
+        while (current.next != null) {
+            nodes.Add(current);
+            current = current.next;
+        }
+        nodes.Add(current);
+        
+        //Console.WriteLine($"fullnode list: [{string.Join(",", nodes.Select(n => n.val))}]");
+        return nodes[nodes.Count - k];
+    }
+}
