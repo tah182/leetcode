fb18f1710b8a6128510391a571556c4bca4a7315
tah182 <tah.tatsumoto@gmail.com>
2022-04-03 22:17:43 -0600

    Time: 376 ms (52.45%), Space: 51.9 MB (13.23%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cs
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cs
@@ -13,11 +13,10 @@ public class Solution {
     public ListNode SwapNodes(ListNode head, int k) {
         var current = head;
         List<ListNode> nodes = new List<ListNode>();
-        while (current.next != null) {
+        while (current != null) {
             nodes.Add(current);
             current = current.next;
         }
-        nodes.Add(current);
         
         var temp = nodes[k - 1].val;
         nodes[k - 1].val = nodes[nodes.Count - k].val;
