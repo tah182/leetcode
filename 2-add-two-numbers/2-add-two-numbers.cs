757cd6a04f2ba7f908edbfe2fbf6dc818878a4dd
tah182 <tah.tatsumoto@gmail.com>
2022-04-04 23:48:04 -0600

    Time: 128 ms (52.36%), Space: 39.6 MB (27.15%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cs
+++ 2-add-two-numbers/2-add-two-numbers.cs
@@ -11,30 +11,17 @@
  */
 public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = SumTwoLists(l1, l2, 0);
-        //Console.WriteLine($"{string.Join("", sum)}");
-        
-//         ListNode head = new ListNode { val = sum[0] };
-//         var current = head;
-//         for (int i = 1; i < sum.Count; i++) {
-//             //Console.WriteLine($"pushing val:{charArray[i]} onto Node val: {current.val}");
-//             current.next = new ListNode { val = sum[i] };
-//             current = current.next;
-//         }
-        return head;
+        return SumTwoLists(l1, l2, 0);
     }
     
     public ListNode SumTwoLists(ListNode l1, ListNode l2, int carryOver) {
         int sum = ((l1?.val ?? 0) + (l2?.val ?? 0)) + carryOver;
     
-        ListNode head = new ListNode();
-        head.val = sum > 9 ? sum - 10 : sum;
+        ListNode head = new ListNode { val = (sum > 9 ? sum - 10 : sum) };
         
-        if (l1?.next == null && l2?.next == null && sum > 9) {
-            head.next = new ListNode { val = 1};
-        }
-        
-        if (l1?.next != null && l2?.next != null) 
+        if (l1?.next == null && l2?.next == null && sum > 9) 
+            head.next = new ListNode { val = 1 };
+        else if (l1?.next != null && l2?.next != null) 
             head.next = SumTwoLists(l1.next, l2.next, sum / 10);
         else if (l2?.next != null)
             head.next = SumTwoLists(null, l2.next, sum / 10);
