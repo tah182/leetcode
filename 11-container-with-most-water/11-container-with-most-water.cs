e88fecb1bb23e62162beb62b72389883fe4ec7da
tah182 <tah.tatsumoto@gmail.com>
2022-04-04 22:14:00 -0600

    Time: 261 ms (45.71%), Space: 44.7 MB (97.47%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cs
+++ 11-container-with-most-water/11-container-with-most-water.cs
@@ -3,11 +3,13 @@ public class Solution {
         int maxVolume = 0;
         var right = height.Length - 1;
         for (var left = 0; left < right; ){ 
-            maxVolume = Math.Max(maxVolume, (right - left) * Math.Min(height[left], height[right]));
-            if (height[left] <= height[right])
+            if (height[left] <= height[right]) {
+                maxVolume = Math.Max(maxVolume, (right - left) * height[left]);
                 left++;
-            else 
+            } else {
+                maxVolume = Math.Max(maxVolume, (right - left) * height[right]);
                 right--;
+            } 
         }
         return maxVolume;
     }
