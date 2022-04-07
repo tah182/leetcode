bce1d885b7fd54b9d93c52e67fc60275bc453a76
tah182 <tah.tatsumoto@gmail.com>
2022-04-06 21:12:15 -0600

    Time: 99 ms (53.70%), Space: 36.6 MB (68.04%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cs
+++ 20-valid-parentheses/20-valid-parentheses.cs
@@ -1,42 +1,30 @@
 public class Solution {
     public bool IsValid(string s) {
-        var queue = new Queue<char>();
-        foreach(char c in s) {
-            queue.Enqueue(c);
-        }
-        
-        //Console.WriteLine($"[{string.Join(",", queue)}]");
         var stack = new Stack<char>();
-        while (queue.Count > 0) {
-            var check = queue.Dequeue();
-            
-            if (stack.Count == 0) {
-                stack.Push(check);
-                continue;
-            } 
-            
-            //Console.WriteLine($"checking [{check}] v [{stack.Peek()}]");
-            switch (check) {
+        stack.Push(s[0]);
+        for (int i = 1; i < s.Length; i++) {
+            //Console.WriteLine($"checking [{s[i]}] v [{stack.Peek()}]");
+            switch (s[i]) {
                 case ')':
-                    if (stack.Peek() == '(') 
+                    if (stack.Any() && stack.Peek() == '(') 
                         stack.Pop();
                     else
-                        stack.Push(check);
+                        stack.Push(s[i]);
                     break;
                 case '}' :
-                    if (stack.Peek() == '{')
+                    if (stack.Any() && stack.Peek() == '{')
                         stack.Pop();
                     else
-                        stack.Push(check);
+                        stack.Push(s[i]);
                     break;
                 case ']' :
-                    if (stack.Peek() == '[')
+                    if (stack.Any() && stack.Peek() == '[')
                         stack.Pop();
                     else 
-                        stack.Push(check);
+                        stack.Push(s[i]);
                     break;
                 default :
-                    stack.Push(check);
+                    stack.Push(s[i]);
                     break;
             }
             //Console.WriteLine($"[{string.Join(",", stack)}]");
