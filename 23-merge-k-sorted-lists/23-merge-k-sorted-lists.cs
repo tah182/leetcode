77978ee2482a6adb579d7b36a95316c57d42e652
tah182 <tah.tatsumoto@gmail.com>
2022-04-06 23:26:22 -0600

    Time: 356 ms (24.75%), Space: 42.7 MB (22.39%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cs
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeKLists(ListNode[] lists) {
+        var leastNodeListIndex = -1;
+        ListNode leastNode = null;
+        for(var i = 0; i < lists.Length; i++) {
+            if (lists[i] == null)
+                continue;
+            
+            if (leastNode == null || lists[i].val < leastNode.val)
+            {
+                leastNode = lists[i];
+                leastNodeListIndex = i;
+            }
+        }
+        
+        if (leastNodeListIndex == -1)
+            return null;
+        
+        lists[leastNodeListIndex] = lists[leastNodeListIndex].next;
+        leastNode.next = MergeKLists(lists);
+        return leastNode;
+    }
+}
