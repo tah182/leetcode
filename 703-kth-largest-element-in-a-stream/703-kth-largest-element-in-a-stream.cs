ce4b186b3b915a593fb9ecc7576a840870524b6a
tah182 <tah.tatsumoto@gmail.com>
2022-04-08 12:49:38 -0600

    Time: 436 ms (17.26%), Space: 49.5 MB (65.85%) - LeetHub

---
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cs
@@ -0,0 +1,34 @@
+public class KthLargest {
+    List<int> nums;
+    int k;
+    
+    public KthLargest(int k, int[] nums) {
+        this.nums = nums.ToList().OrderByDescending(num => num).ToList();
+        this.k = k;
+    }
+    
+    public int Add(int val) {
+        if (nums.Count == 0)
+        {
+            nums.Add(val);
+        } else {
+            for (int i = 0; i < nums.Count; i++) {
+                if (val > nums[i]) {
+                    nums.Insert(i, val);
+                    break;
+                } else if (i == nums.Count - 1) {
+                    nums.Add(val);
+                    break;
+                }
+            }    
+        }
+        
+        return nums[k - 1];
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.Add(val);
+ */
