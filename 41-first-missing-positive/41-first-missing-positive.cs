3302a0036a8b87eb08802f0445ed4197a1ed9fff
tah182 <tah.tatsumoto@gmail.com>
2022-04-13 01:21:02 -0600

    Time: 252 ms (82.08%), Space: 68.7 MB (31.80%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cs
+++ 41-first-missing-positive/41-first-missing-positive.cs
@@ -2,7 +2,7 @@ public class Solution {
     public int FirstMissingPositive(int[] nums) {
         Array.Sort(nums);
         var smallestMissing = 1;
-        foreach(var num in nums) {
+        foreach(var num in nums.Where(n => n > 0)) {
             if (num == smallestMissing)
                 smallestMissing++;
         }
