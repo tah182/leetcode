df562975abbf0ef1c2e40769651475b57f9804b2
tah182 <tah.tatsumoto@gmail.com>
2022-04-14 00:10:52 -0600

    Time: 136 ms (82.55%), Space: 42.4 MB (19.04%) - LeetHub

---
+++ 27-remove-element/27-remove-element.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int cursor = 0;
+        for(int i = 0; i < nums.Length; i++) {
+            if (nums[i] != val) {
+                nums[cursor] = nums[i];
+                cursor++;
+            }
+        }
+        return cursor;
+    }
+}
