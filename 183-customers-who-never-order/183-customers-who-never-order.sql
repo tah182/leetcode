ef1fb699c7a00adfab6a66b3944a0068cb74fdb2
tah182 <tah.tatsumoto@gmail.com>
2022-04-15 16:36:51 -0600

    Time: 557 ms (93.04%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,6 +1,10 @@
 /* Write your T-SQL query statement below */
+;WITH o AS (
+    SELECT DISTINCT customerId customerId
+    FROM Orders
+)
 SELECT c.Name Customers
 FROM Customers c
-LEFT JOIN Orders o
+LEFT JOIN o
 ON c.id = o.customerId
-WHERE o.id IS NULL
+WHERE o.customerId IS NULL
