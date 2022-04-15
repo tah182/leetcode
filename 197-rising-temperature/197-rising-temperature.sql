02ad88cde4588d7aa3bb5ecd61998837018dc388
tah182 <tah.tatsumoto@gmail.com>
2022-04-14 23:21:15 -0600

    Time: 737 ms (93.06%), Space: 0B (100.00%) - LeetHub

--- 197-rising-temperature/197-rising-temperature.sql
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -1,5 +1,9 @@
 /* Write your T-SQL query statement below */
+;WITH w2 AS (
+    SELECT id, temperature, DateAdd(day, -1, recordDate) recordDate
+    FROM Weather
+)
 SELECT w2.id FROM Weather w1
-INNER JOIN Weather w2
-ON w1.recordDate = DateAdd(day, -1, w2.recordDate)
+INNER JOIN w2
+ON w1.recordDate = w2.recordDate
 WHERE w2.temperature > w1.temperature
