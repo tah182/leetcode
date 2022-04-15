5c6c46f5b64f23546b2e6096574ca63405d4d474
tah182 <tah.tatsumoto@gmail.com>
2022-04-15 14:59:36 -0600

    Time: 93 ms (71.52%), Space: 47.1 MB (5.02%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cs
+++ 125-valid-palindrome/125-valid-palindrome.cs
@@ -3,8 +3,6 @@ public class Solution {
     public bool IsPalindrome(string s) {
         Regex regex = new Regex("[^a-zA-Z0-9]");
         s = regex.Replace(s, String.Empty).ToLower();
-        // Console.WriteLine($"{s}");
-        // Console.WriteLine($"{string.Join("", s.ToCharArray().Reverse())}");
         return s == string.Join("", s.ToCharArray().Reverse());
     }
 }
