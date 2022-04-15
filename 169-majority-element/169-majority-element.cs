cd2dee583c24f0d48dd7f65d5b58866993ca4a9c
tah182 <tah.tatsumoto@gmail.com>
2022-04-15 17:00:34 -0600

    Time: 113 ms (78.60%), Space: 44 MB (6.92%) - LeetHub

--- 169-majority-element/169-majority-element.cs
+++ 169-majority-element/169-majority-element.cs
@@ -1,11 +1,11 @@
 public class Solution {
     public int MajorityElement(int[] nums) {
         Dictionary<int, int> finds = new Dictionary<int, int>();
-        foreach (var num in nums) {
-            if (finds.ContainsKey(num)) 
-                finds[num]++;
+        for(int i = 0; i < nums.Length; i++) {
+            if (finds.ContainsKey(nums[i])) 
+                finds[nums[i]]++;
             else
-                finds.Add(num, 1);
+                finds.Add(nums[i], 1);
         }
         foreach (var keyValue in finds) {
             if (keyValue.Value > nums.Length / 2)
