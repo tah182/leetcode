701f0243dc5bacd113b8e6d8e89046c95b57c766
tah182 <tah.tatsumoto@gmail.com>
2022-04-16 16:49:24 -0600

    Time: 60 ms (91.80%), Space: 38.6 MB (17.67%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cs
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cs
@@ -1,9 +1,8 @@
 public class Solution {
     public int TitleToNumber(string columnTitle) {
-        int sum = 0, multiplier = 0;
-        foreach(var c in columnTitle.ToCharArray().Reverse()) {
-            sum += (int)((c - 'A' + 1) * Math.Pow(26, multiplier));
-            multiplier++;
+        int sum = 0;
+        for (int i = columnTitle.Length - 1; i >= 0; i--) {
+            sum += (int)((columnTitle[i] - 'A' + 1) * Math.Pow(26, columnTitle.Length - 1 - i));
         }
         return sum;
     }
