d091e514dac219d61b380cb3e7e654a8bf7c506b
tah182 <tah.tatsumoto@gmail.com>
2022-04-17 11:52:02 -0600

    Time: 110 ms (61.32%), Space: 42.1 MB (22.55%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cs
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cs
@@ -15,7 +15,7 @@ public class Solution {
             head = head.next;
         }
         
-        var current = head;
+        ListNode current = head;
         while (current != null && current.next != null) {
             if (current.next.val == val)
                 current.next = current.next.next;
