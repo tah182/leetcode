d7a73876d278c9f4aa97177a7500f1127f9c47b4
tah182 <tah.tatsumoto@gmail.com>
2022-04-17 00:52:34 -0600

    Time: 84 ms (79.55%), Space: 39.5 MB (7.68%) - LeetHub

--- 67-add-binary/67-add-binary.cs
+++ 67-add-binary/67-add-binary.cs
@@ -5,20 +5,22 @@ public class Solution {
         
         string result = "";
         var carry = 0;
-        int add = 0;
         while (aIndex >= 0 || bIndex >= 0) {
-            add = (aIndex >= 0 ? (a[aIndex] - '0') : 0) + 
-                    (bIndex >= 0 ? (b[bIndex] - '0') : 0) + carry;
-            
-            if (add == 3) {
-                result = "1" + result;
-                carry = 1;
-            } else if (add == 2) {
-                result = "0" + result;
-                carry = 1;
-            } else {
-                result = (add).ToString() + result;
-                carry = 0;
+            switch ((aIndex >= 0 ? (a[aIndex] - '0') : 0) + 
+                    (bIndex >= 0 ? (b[bIndex] - '0') : 0) + carry) {
+                case 3:
+                    result = "1" + result;
+                    carry = 1;    
+                    break;
+                case 2:
+                    result = "0" + result;
+                    carry = 1;    
+                    break;
+                default:
+                    result = ((aIndex >= 0 ? (a[aIndex] - '0') : 0) + 
+                    (bIndex >= 0 ? (b[bIndex] - '0') : 0) + carry).ToString() + result;
+                    carry = 0;
+                    break;
             }
                 
             aIndex--;
