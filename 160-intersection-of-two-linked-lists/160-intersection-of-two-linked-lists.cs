8e103fba717592f86148693b27e55aef01319c3b
tah182 <tah.tatsumoto@gmail.com>
2022-04-17 00:05:01 -0600

    Time: 121 ms (83.04%), Space: 51.2 MB (5.04%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cs
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cs
@@ -8,15 +8,36 @@
  */
 public class Solution {
     public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
-        while (headA != null) {
-            var currentB = headB;
-            while (currentB != null) {
-                if (headA == currentB)
-                    return headA;
-                currentB = currentB.next;
-            }
-            headA = headA.next;
+        int aLength = 0;
+        int bLength = 0;
+        
+        var currentA = headA;
+        var currentB = headB;
+        
+        while (currentA != null) {
+            aLength++;
+            currentA = currentA.next;
         }
-        return null;
+        
+        while (currentB != null) {
+            bLength++;
+            currentB = currentB.next;
+        }
+        
+        while (aLength - bLength > 0) {
+            headA = headA.next;
+            aLength--;
+        }
+        
+        while (bLength - aLength > 0) {
+            headB = headB.next;
+            bLength--;
+        }
+        
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
     }
 }
