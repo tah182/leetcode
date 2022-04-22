5c24b9b18c83a46ad7bdf00fead92fd1d63a7925
tah182 <tah.tatsumoto@gmail.com>
2022-04-21 22:09:01 -0600

    Time: 427 ms (40.79%), Space: 58.2 MB (28.21%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cs
+++ 706-design-hashmap/706-design-hashmap.cs
@@ -1,43 +1,20 @@
 public class MyHashMap {
-    List<KeyVal>[] map;
+    int?[] map;
     public MyHashMap() {
-        map = new List<KeyVal>[19];
+        map = new int?[1000001];
     }
     
     public void Put(int key, int value) {
-        var temp = map[key % 19];
-        if (temp == null)
-            map[key % 19] = new List<KeyVal> { new KeyVal(key, value ) };
-        else {
-            if (temp.Any(k => k.Key == key)) 
-                map[key % 19].Single(k => k.Key == key).Val = value;
-            else
-                map[key % 19].Add(new KeyVal(key, value));
-        }
+        map[key] = value;
     }
     
     public int Get(int key) {
-        var temp = map[key % 19];
-        if (temp != null && temp.Any(k => k.Key == key)) 
-            return temp.Single(k => k.Key == key).Val;
-        
-        return -1;
+        var val = map[key];
+        return val ?? -1;
     }
     
     public void Remove(int key) {
-        var temp = map[key % 19];
-        if (temp != null && temp.Any(k => k.Key == key)) {
-            temp.Remove(temp.Single(k => k.Key == key));
-        }
-    }
-    
-    class KeyVal {
-        public KeyVal(int key, int val) {
-            this.Key = key;
-            this.Val = val;
-        }
-        public int Key {get;set;}
-        public int Val {get;set;}
+        map[key] = null;
     }
 }
 
