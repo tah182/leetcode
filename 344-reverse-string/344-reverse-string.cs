7db7f8014af23f3653655b38d981ab8077a62de3
tah182 <tah.tatsumoto@gmail.com>
2022-04-23 22:28:18 -0600

    Time: 267 ms (72.42%), Space: 48.2 MB (47.62%) - LeetHub

--- 344-reverse-string/344-reverse-string.cs
+++ 344-reverse-string/344-reverse-string.cs
@@ -1,11 +1,11 @@
 public class Solution {
     public void ReverseString(char[] s) {
-        int left = 0;
+        int left = 0, right = s.Length - 1;
         char temp;
-        for (int right = s.Length - 1; left < right; right--, left++) {
+        while(left < right) {
             temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
+            s[left++] = s[right];
+            s[right--] = temp;
         }
     }
 }
