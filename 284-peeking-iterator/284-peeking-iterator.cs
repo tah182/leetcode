8a680386beca8e136e928dbb0306369cac4f4fa7
tah182 <tah.tatsumoto@gmail.com>
2022-04-24 23:53:54 -0600

    Time: 113 ms (91.07%), Space: 41.6 MB (16.07%) - LeetHub

---
+++ 284-peeking-iterator/284-peeking-iterator.cs
@@ -0,0 +1,29 @@
+// C# IEnumerator interface reference:
+// https://docs.microsoft.com/en-us/dotnet/api/system.collections.ienumerator?view=netframework-4.8
+
+class PeekingIterator {
+    IEnumerator<int> head;
+    bool isEnd;
+    // iterators refers to the first element of the array.
+    public PeekingIterator(IEnumerator<int> iterator) {
+        head = iterator;
+        isEnd = true;
+    }
+    
+    // Returns the next element in the iteration without advancing the iterator.
+    public int Peek() {
+        return head.Current;
+    }
+    
+    // Returns the next element in the iteration and advances the iterator.
+    public int Next() {
+        var val = head.Current;
+        isEnd = head.MoveNext();
+        return val;
+    }
+    
+    // Returns false if the iterator is refering to the end of the array of true otherwise.
+    public bool HasNext() {
+		return isEnd;
+    }
+}
