2cf211cc824676c4cb325bc6081174f2b6ded135
tah182 <tah.tatsumoto@gmail.com>
2022-04-24 23:55:06 -0600

    Time: 188 ms (33.93%), Space: 40.5 MB (48.21%) - LeetHub

--- 284-peeking-iterator/284-peeking-iterator.cs
+++ 284-peeking-iterator/284-peeking-iterator.cs
@@ -3,11 +3,11 @@
 
 class PeekingIterator {
     IEnumerator<int> head;
-    bool isEnd;
+    bool hasNext;
     // iterators refers to the first element of the array.
     public PeekingIterator(IEnumerator<int> iterator) {
         head = iterator;
-        isEnd = true;
+        hasNext = true;
     }
     
     // Returns the next element in the iteration without advancing the iterator.
@@ -18,12 +18,12 @@ class PeekingIterator {
     // Returns the next element in the iteration and advances the iterator.
     public int Next() {
         var val = head.Current;
-        isEnd = head.MoveNext();
+        hasNext = head.MoveNext();
         return val;
     }
     
     // Returns false if the iterator is refering to the end of the array of true otherwise.
     public bool HasNext() {
-		return isEnd;
+		return hasNext;
     }
 }
