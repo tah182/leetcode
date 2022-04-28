4d8691e351ba1dd8ce993c2238b3b912556b499a
tah182 <tah.tatsumoto@gmail.com>
2022-04-27 22:59:03 -0600

    Time: 243 ms (100.00%), Space: 46.9 MB (54.55%) - LeetHub

---
+++ 1447-simplified-fractions/1447-simplified-fractions.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public IList<string> SimplifiedFractions(int n) {
+        var results = new List<string>();
+        if (n <= 1 )
+            return results;
+        
+        while (n > 1) {
+            for (int top = n - 1; top > 0; top--) {
+                // Console.WriteLine($"top[{top}]n[{n}]{gcd(top, n)}");
+                if (top == 1)
+                    results.Add($"{top}/{n}");
+                else if (gcd(top, n) == 1)
+                    results.Add($"{top}/{n}");
+            }
+            n--;
+        }
+        return results;
+    }
+    
+    static int gcd(int a, int b)
+    {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+}
