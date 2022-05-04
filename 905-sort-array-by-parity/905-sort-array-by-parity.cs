80ec67b5745f876f81ee69e8cea41d3db6051cec
tah182 <tah.tatsumoto@gmail.com>
2022-05-03 19:03:53 -0600

    Time: 295 ms (5.05%), Space: 45.5 MB (24.89%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cs
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cs
@@ -1,19 +1,22 @@
 public class Solution {
     public int[] SortArrayByParity(int[] nums) {
-        Stack<int> odds = new Stack<int>();
-        Stack<int> evens = new Stack<int>();
+        var odds = new Queue<(int index, int value)>();
+        int temp = 0;
+        (int index, int value) odd;
         for (int i = 0 ; i < nums.Length; i++) {
-            if (nums[i] % 2 == 0)
-                evens.Push(nums[i]);
-            else
-                odds.Push(nums[i]);
+            if (nums[i] % 2 == 0) {
+                if (odds.Count == 0)
+                    continue;
+                else {
+                    odd = odds.Dequeue();
+                    // Console.WriteLine($"[{string.Join(",", nums)}]--swap{nums[odd.index]}={nums[i]}");
+                    nums[odd.index] = nums[i];
+                    nums[i] = odd.value;
+                    i--;
+                }
+            } else
+                odds.Enqueue((i, nums[i]));
         }
-        
-        for (int i = 0; i < nums.Length; i++)
-            if (evens.Count > 0)
-                nums[i] = evens.Pop();
-            else
-                nums[i] = odds.Pop();
         
         return nums;
     }
