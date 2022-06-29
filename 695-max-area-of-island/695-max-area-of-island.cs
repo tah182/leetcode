4d3d88ea9c91a37b475a79b602138a37312e36e3
tah182 <tah.tatsumoto@gmail.com>
2022-06-29 13:20:05 -0600

    Time: 159 ms (35.41%), Space: 39.7 MB (35.76%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cs
+++ 695-max-area-of-island/695-max-area-of-island.cs
@@ -2,30 +2,32 @@ public class Solution {
     private List<(int x, int y)> adjacent = new List<(int x, int y)> {
         (0, 1), (0, -1), (-1, 0), (1, 0)
     };
+    private int[][] _grid;
     
     public int MaxAreaOfIsland(int[][] grid) {
         var maxSize = 0;
+        _grid = grid;
         for (int i = 0; i < grid.Length; i++) {
             for(int j = 0; j < grid[0].Length; j++) {
-                if (grid[i][j] == 1) {
-                    maxSize = Math.Max(maxSize, IslandSize(i, j, ref grid)); 
+                if (_grid[i][j] == 1) {
+                    maxSize = Math.Max(maxSize, IslandSize(i, j)); 
                 }
             }
         }
         return maxSize;
     }
     
-    private int IslandSize(int r, int c, ref int[][] grid) {
-        if (grid[r][c] == 0) return 0;
+    private int IslandSize(int r, int c) {
+        if (_grid[r][c] == 0) return 0;
         
         var islandSize = 1;
-        grid[r][c] = 0;
+        _grid[r][c] = 0;
         foreach (var adj in adjacent) {
             if (r + adj.x >= 0
-                && r + adj.x < grid.Length
+                && r + adj.x < _grid.Length
                && c + adj.y >= 0
-               && c + adj.y < grid[0].Length)
-                islandSize += IslandSize(r + adj.x, c + adj.y, ref grid);
+               && c + adj.y < _grid[0].Length)
+                islandSize += IslandSize(r + adj.x, c + adj.y);
         }
         return islandSize;
     }
