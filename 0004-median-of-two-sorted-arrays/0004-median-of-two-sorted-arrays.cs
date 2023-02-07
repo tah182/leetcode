aafd60acf8621471d9a2be6da64f8720a2f77885
tah182 <tah.tatsumoto@gmail.com>
2023-02-06 22:48:36 -0700

    Time: 111 ms (64.27%), Space: 52.3 MB (15.34%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
@@ -1,12 +1,10 @@
 public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
-        var size = (nums1.Length + nums2.Length);
-        
-        if (size == 1) 
+        if (nums1.Length + nums2.Length == 1) 
             return nums1.Length == 0 ? nums2[0] : nums1[0];
         
         int nums1Ptr = 0, nums2Ptr = 0;
-        var nums = new int[size / 2 + 1];
+        var nums = new int[(nums1.Length + nums2.Length) / 2 + 1];
         for (int i = 0; i < nums.Length; i++) {
             if (nums1Ptr >= nums1.Length) {
                 nums[i] = nums2[nums2Ptr];
@@ -30,7 +28,7 @@ public class Solution {
         }
         
         // Console.WriteLine($"{string.Join(",", nums.ToList())}");
-        if (size % 2 == 0)
+        if ((nums1.Length + nums2.Length) % 2 == 0)
             return (nums[nums.Length - 2] + nums[nums.Length - 1]) / 2.0;
         else
             return nums[nums.Length - 1];
