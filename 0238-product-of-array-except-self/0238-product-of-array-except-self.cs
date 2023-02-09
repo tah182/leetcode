228d47f4fa6d327327366d92a9b8af9a29a2d570
tah182 <tah.tatsumoto@gmail.com>
2023-02-09 11:44:39 -0700

    Time: 162 ms (94.93%), Space: 53.9 MB (47.32%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        var temp = 1;
+        
+        var output = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++) {
+            output[i] = temp;
+            temp *= nums[i];
+        }
+        
+        temp = 1;
+        for (int i = nums.Length - 1; i >= 0; i--) {
+            output[i] = output[i] * temp;
+            temp *= nums[i];
+        }
+        
+        return output;
+    }
+}
