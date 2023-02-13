21ee159fa155af6e6b825a63877322c2c7c34131
tah182 <tah.tatsumoto@gmail.com>
2023-02-12 17:14:03 -0700

    Time: 27 ms (77.26%), Space: 26.7 MB (15.45%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cs
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cs
@@ -1,19 +1,9 @@
 public class Solution {
     public int CountOdds(int low, int high) {
         var count = 0;
-        if (low % 2 == 1) {
+        if (low % 2 == 0) 
             low++;
-            count++;
-        }
         
-        if (high % 2 == 1 && high > low) {
-            high--;
-            count++;
-        }
-        
-        if (low < high)
-            count += (high - low) / 2;
-        
-        return count;
+        return low > high ? 0 : (high - low) / 2 + 1;
     }
 }
