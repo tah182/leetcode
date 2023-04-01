12dcc8d9a537dc9010cfd7cce4c9a891ea2e5df5
tah182 <tah.tatsumoto@gmail.com>
2023-04-01 14:23:16 -0600

    Time: 123 ms (61.47%), Space: 50.9 MB (62.36%) - LeetHub

--- 0704-binary-search/0704-binary-search.cs
+++ 0704-binary-search/0704-binary-search.cs
@@ -1,15 +1,15 @@
 public class Solution {
     public int Search(int[] nums, int target) {
-        int left = 0, right = nums.Length - 1;
+        int left = 0, right = nums.Length - 1, mid;
         while (left <= right) {
-            var mod = (right - left) / 2 + left;
-            if (target == nums[mod])
-                return mod;
+            mid = (right - left) / 2 + left;
             
-            if (target < nums[mod])
-                right = mod - 1;
+            if (target < nums[mid])
+                right = mid - 1;
+            else if (target > nums[mid])
+                left = mid + 1;
             else
-                left = mod + 1;
+                return mid;
         }
         
         return -1;
