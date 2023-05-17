c74368d2b1fac18c08ddc98d031221ca724bb9c0
tah182 <tah.tatsumoto@gmail.com>
2023-05-16 23:25:25 -0600

    Time: 283 ms (56.64%), Space: 55.5 MB (62.83%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cs
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cs
@@ -11,20 +11,18 @@
  */
 public class Solution {
     public int PairSum(ListNode head) {
-        if (head == null) return 0;
+        var values = new List<int>();
         
-        Dictionary<int, int> twins = new Dictionary<int, int>();
-        int size = 0;
         while (head != null) {
-            twins[size] = head.val;
-            size++;
-            head = head.next;            
+            values.Add(head.val);
+            head = head.next;
         }
         
-        int max = int.MinValue;
-        foreach (var keyval in twins) {
-            var sum = keyval.Value + twins[size - 1 - keyval.Key];
-            max = Math.Max(max, sum);
+        int i = 0, j = values.Count() - 1, max = 0;
+        while (i < j) {
+            max = Math.Max(max, values[i] + values[j]);
+            i++;
+            j--;
         }
         
         return max;
