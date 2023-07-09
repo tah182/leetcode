59253a65eca10962a2229b152695619893f3a323
tah182 <tah.tatsumoto@gmail.com>
2023-07-08 23:45:50 -0600

    Time: 80 ms (68.47%), Space: 38.4 MB (35.71%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cs
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cs
@@ -1,17 +1,15 @@
 public class Solution {
     public string MergeAlternately(string word1, string word2) {
         var result = string.Empty;
-        for (int i = 0; i < Math.Max(word1.Length, word2.Length); i++) {
-            if (i >= word1.Length) 
-                return result + word2.Substring(i, word2.Length - i);
-            if (i >= word2.Length)
-                return result + word1.Substring(i , word1.Length - i);
-            
+        for (int i = 0; i < Math.Min(word1.Length, word2.Length); i++) {
             result += $"{word1[i]}{word2[i]}";
-            
-            // Console.WriteLine($"word1[{i}]={word1[i]} || word2[{i}]={word2[i]} || result [{result}]");
         }
         
+        if (word1.Length < word2.Length) 
+            result += word2.Substring(word1.Length, word2.Length - word1.Length);
+        if (word2.Length < word1.Length)
+            result += word1.Substring(word2.Length, word1.Length - word2.Length);
+            
         return result;
     }
 }
