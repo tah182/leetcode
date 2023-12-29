dd3fcfc58a6e9ba9e489670c2a361e41035d475d
tah182 <tah.tatsumoto@gmail.com>
2023-12-28 22:10:14 -0700

    Time: 101 ms (5.62%), Space: 39.5 MB (5.62%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cs
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cs
@@ -1,5 +1,12 @@
 public class Solution {
     public int PercentageLetter(string s, char letter) {
-        return (int)(s.Count(c => c == letter) * 1.0 / s.Length * 100);
+        int counter = 0;
+        int count = 0;
+        for (int i = 0; i < s.Length; i++) {
+            count++;
+            counter += s[i] == letter ? 1 : 0;
+        }
+        
+        return (int)(counter * 1.0 / count * 100);
     }
 }
