ad7e86d1ed932bca9b725e17489aaa4739a672c9
tah182 <tah.tatsumoto@gmail.com>
2024-06-22 00:06:57 -0600

    Time: 120 ms (23.79%), Space: 47.8 MB (89.91%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cs
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cs
@@ -1,14 +1,13 @@
 public class Solution {
     public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
-        var max = int.MinValue;
-        foreach (var x in candies) 
-            max = Math.Max(max, x);
+        int greatest = -1;
+        foreach (var kid in candies)
+            greatest = Math.Max(kid, greatest);
         
-        var returnVal = new List<bool>();
-        foreach (var x in candies) {
-            returnVal.Add(x + extraCandies >= max);
-        }
+        var result = new List<bool>();
+        foreach (var kid in candies)
+            result.Add(kid + extraCandies >= greatest);
         
-        return returnVal;
+        return result;
     }
 }
