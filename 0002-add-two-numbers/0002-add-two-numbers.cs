a16ea7b8a01b96be2058df1ffcd48f3b704845fa
tah182 <tah.tatsumoto@gmail.com>
2024-06-21 23:49:07 -0600

    Time: 84 ms (26.58%), Space: 51.3 MB (63.73%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cs
+++ 0002-add-two-numbers/0002-add-two-numbers.cs
@@ -15,41 +15,28 @@ public class Solution {
         var current = result;
         
         var carry = 0;
-        do {
-            if (l1 == null && l2 != null) {
-                current.val = addNumbers(l2.val, carry, out carry);
-                l2 = l2.next;
-            } else if (l2 == null && l1 != null) {
-                current.val = addNumbers(l1.val, carry, out carry);
+        while (l1 != null || l2 != null || carry == 1) {
+            var sum = 0;
+            if (l1 != null) {
+                sum += l1.val;
                 l1 = l1.next;
-            } else if (l2 != null && l1 != null) {
-                current.val = addNumbers(l1.val + carry, l2.val, out carry);
-                l1 = l1.next;
-                l2 = l2.next;
-            } else if (carry == 1) {
-                current.val = carry;   
-                carry = 0;
-            }
+            } 
             
-            if (l1 != null || l2 != null || carry == 1)
-            {
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            } 
+            
+            sum += carry;
+            current.val = sum % 10;
+            carry = sum / 10;
+            
+            if (l1 != null || l2 != null || carry == 1) {
                 current.next = new ListNode();
                 current = current.next;
             }
-        } while (l1 != null || l2 != null || carry == 1);
+        } 
         
-        return result;
-    }
-    
-    private int addNumbers(int i, int k, out int carry) {
-        var result = i + k;
-        
-        if (result > 9) { 
-            carry = 1;
-            return result - 10;
-        }
-        
-        carry = 0;
         return result;
     }
 }
