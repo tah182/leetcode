d81cadaecbf556d47f61f2f109324c3571e85b44
tah182 <tah.tatsumoto@gmail.com>
2024-07-03 15:31:38 -0600

    Time: 149 ms (70.42%), Space: 87.1 MB (19.72%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cs
+++ 0049-group-anagrams/0049-group-anagrams.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public IList<IList<string>> GroupAnagrams(string[] strs) {
-        var anagrams = new Dictionary<string, List<string>>();
+        var anagrams = new Dictionary<string, IList<string>>();
         foreach (var str in strs) {
             var minLex = new string(str.ToCharArray().OrderBy(x => x).ToArray());
             if (anagrams.ContainsKey(minLex))
@@ -9,10 +9,6 @@ public class Solution {
                 anagrams.Add(minLex, new List<string> { str });
         }
 
-        var result = new List<IList<string>>();
-        foreach (var keyVal in anagrams) {
-            result.Add(keyVal.Value);
-        }
-        return result;
+        return anagrams.Select(x => x.Value).ToList();
     }
 }
