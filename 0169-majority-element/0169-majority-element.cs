da0bbc2c758a564efe295c1f6fc740d2df45dbc8
tah182 <tah.tatsumoto@gmail.com>
2024-07-05 22:03:19 -0600

    Time: 120 ms (41.03%), Space: 48.9 MB (23.85%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        var dic = new Dictionary<int, int>();
+        foreach (var num in nums) {
+            if (dic.TryGetValue(num, out var t)) {
+                dic[num]++;
+                if (dic[num] > nums.Count() / 2)
+                    return num;
+            }
+            else
+                dic.Add(num, 1);
+        }
+        return nums[0];
+    }
+}
