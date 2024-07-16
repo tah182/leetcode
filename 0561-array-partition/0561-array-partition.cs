1390d0b55a0ccf2fc644d9f4f13a1ac47da6a922
tah182 <tah.tatsumoto@gmail.com>
2024-07-15 21:29:24 -0600

    Time: 184 ms (5.29%), Space: 55.1 MB (5.29%) - LeetHub

--- 0561-array-partition/0561-array-partition.cs
+++ 0561-array-partition/0561-array-partition.cs
@@ -1,11 +1,9 @@
 public class Solution {
     public int ArrayPairSum(int[] nums) {
         nums = nums.OrderBy(x => x).ToArray();
-        int max = 0, i = 0, j = 1;
-        while (i < nums.Length) {
-            max = max + Math.Min(nums[i], nums[j]);
-            i += 2;
-            j += 2;
+        int max = 0;
+        for (int i = 0; i < nums.Length - 1; i +=2) {
+            max = max + Math.Min(nums[i], nums[i + 1]);
         }
         return max;
     }
