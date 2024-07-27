6f5139ed8b98d72696197d202ffd8696ca7959c2
tah182 <tah.tatsumoto@gmail.com>
2024-07-26 18:29:32 -0600

    Time: 65 ms (41.7%), Space: 41.9 MB (33.07%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cs
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cs
@@ -11,55 +11,43 @@
  */
 public class Solution {
     public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 is null && list2 is null)
+            return null;
+        
         ListNode head = null;
         var current = head;
-        while (list1 != null || list2 != null) {
-            if (list1 == null) {
-                if (head == null) return list2;
-                
-                current.next = list2;
-                return head;
+        while (list1 is not null || list2 is not null) {
+            ListNode next;
+            if (list1 is null) {
+                if (head == null)
+                    head = list2;
+                else
+                    current.next = list2;
+                break;
             }
-            if (list2 == null) {
-                if (head == null) return list1;
-                
-                current.next = list1;
-                return head;
+            if (list2 is null) {
+                if (head == null)
+                    head = list1;
+                else
+                    current.next = list1;
+                break;
             }
-                
-            if (list1.val <= list2.val) {
-                if (current == null) {
-                    head = new ListNode(list1.val);
-                    current = head;
-                } else {
-                    current.next = new ListNode(list1.val);
-                    current = current.next;
-                }
+
+            if (list1.val < list2.val) {
+                next = list1;
                 list1 = list1.next;
             } else {
-                if (current == null) {
-                    head = new ListNode(list2.val);
-                    current = head;
-                } else {
-                    current.next = new ListNode(list2.val);
-                    current = current.next;
-                }
+                next = list2;
                 list2 = list2.next;
             }
+            
+            if (current == null)
+                current = head = next;
+            else {
+                current.next = next;
+                current = current.next;
+            }
         }
-        
         return head;
     }
 }
-/**
-[1,2,4]
-[1,3,4]
-
-head 1
-current = head
-current 1
-[2,4]
-[1,3,4]
-
-current
-**/
