ed957d82b9eaf70cf436a7c8dcd012ea8be6e515
tah182 <tah.tatsumoto@gmail.com>
2024-08-19 10:52:45 -0600

    Time: 82 ms (77.16%), Space: 49.3 MB (63.49%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cs
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int CountNodes(TreeNode root) {
+        if (root is null) 
+            return 0;
+
+        var sum = CountNodes(root.left);
+        sum += CountNodes(root.right);
+
+        return sum + 1;
+    }
+}
