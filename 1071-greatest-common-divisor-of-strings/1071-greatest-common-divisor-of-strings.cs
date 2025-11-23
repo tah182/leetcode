80333cf56dce24c8d5d7876d59a0601609a63cee
tah182 <tah.tatsumoto@gmail.com>
2025-11-22 23:46:13 -0700

    Time: 1 ms (36.57%), Space: 41.6 MB (51.24%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cs
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cs
@@ -1,15 +1,7 @@
 public class Solution {
-    private static int gcd(int x, int y) {
-        if (y == 0) 
-            return x;
-        return gcd(y, x % y);
-    }
-    
     public string GcdOfStrings(string str1, string str2) {
-        if (!(str1 + str2 == str2 + str1)) 
-            return string.Empty;
-        
-        int gcdLength = gcd(str1.Length, str2.Length);
-        return str1.Substring(0, gcdLength);
+        return (str1 + str2 == str2 + str1) 
+           ? str1.Substring(0, (int)BigInteger.GreatestCommonDivisor(str1.Length, str2.Length)) 
+           : "";
     }
 }
