78b40e4b1ea658d3dc67fe4eb5821379f7c9d8f6
tah182 <tah.tatsumoto@gmail.com>
2025-11-23 23:28:50 -0700

    Time: 0 ms (100%), Space: 41.2 MB (97.27%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cs
+++ 0392-is-subsequence/0392-is-subsequence.cs
@@ -2,7 +2,7 @@ public class Solution {
     public bool IsSubsequence(string s, string t) {
         int sPointer = 0, tPointer = 0;
         while (sPointer < s.Length && tPointer < t.Length) {
-            Console.WriteLine($"s={s} sPointer={s[sPointer]} tPointer={t[tPointer]}");
+            // Console.WriteLine($"s={s} sPointer={s[sPointer]} tPointer={t[tPointer]}");
             if (s[sPointer] == t[tPointer]) {
                 sPointer++;
             }
