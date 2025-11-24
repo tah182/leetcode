fff0f37b9ae895a3eeea055fa574041aca6d037a
tah182 <tah.tatsumoto@gmail.com>
2025-11-23 22:42:32 -0700

    Time: 1 ms (100%), Space: 42.2 MB (50.29%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
@@ -1,12 +1,9 @@
 public class Solution {
     public string ReverseWords(string s) {
-        List<string> words = s.Split(" ").ToList();
-        StringBuilder returnVal = new StringBuilder();
-        for (int i = words.Count - 1; i >= 0; i--) {
-            var trimmedWord = words[i].Trim();
-            returnVal.Append(trimmedWord);
-            if (trimmedWord.Length > 0) returnVal.Append(" ");
-        }
-        return returnVal.ToString().Trim();
+        string str = s.Trim();
+        string[] words = str.Split(new char[] {' '},StringSplitOptions.RemoveEmptyEntries);
+        Array.Reverse(words);
+        string res = string.Join(" ",words);
+        return res;
     }
 }
