d30f3aa8ff0ca305daa40e92d18ec4bf87336afa
tah182 <tah.tatsumoto@gmail.com>
2025-11-23 22:05:08 -0700

    Time: 1 ms (100%), Space: 49.3 MB (80.65%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cs
+++ 0605-can-place-flowers/0605-can-place-flowers.cs
@@ -5,22 +5,7 @@ public class Solution {
         for (int i = 0; i < flowerbed.Length; i++) {
             if (n == 0) break;
             if (flowerbed[i] == 0) {
-                if (i == 0) {
-                    if (flowerbed[i + 1] == 0) {
-                        flowerbed[i] = 1;
-                        n--;
-                    }
-                    continue;
-                }
-                if (i == flowerbed.Length - 1) {
-                    if (flowerbed[i - 1] == 0) {
-                        flowerbed[i] = 1;
-                        n--;
-                    }
-                    continue;
-                }
-
-                if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
+                if ((i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0)) {
                     flowerbed[i] = 1;
                     n--;
                 }
