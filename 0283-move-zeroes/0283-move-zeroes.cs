bda31390b6deeb680c5a538b6f28c0b2e4101272
tah182 <tah.tatsumoto@gmail.com>
2025-11-23 23:22:52 -0700

    Time: 1 ms (95.59%), Space: 58.4 MB (76.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cs
+++ 0283-move-zeroes/0283-move-zeroes.cs
@@ -1,20 +1,23 @@
 public class Solution {
     public void MoveZeroes(int[] nums) {
-        int n = nums.Length;
-        int j = -1;
-        int i = 0; 
-    
-        while (i < n) {
-            //first occurence of 0
-            if (nums[i] == 0 && j == -1) 
-                j = i;
-            //has zero in the array before a valid digit, hence swap 
-            else if (nums[i] != 0 && j != -1) {
-                nums[j] = nums[i];
-                nums[i] = 0; 
-                j++;  
+        int left = 0, right = 1;
+        while (right < nums.Length && left < nums.Length) {
+            // Console.WriteLine($"current progress: {string.Join(", ", nums)}, left: {nums[left]} right: {nums[right]}");
+            if (nums[left] != 0) {
+                left++;
+                continue;
             }
-            i++;
+            if (nums[right] == 0 || right <= left) {
+                right++;
+                continue;
+            }
+
+            if (nums[left] == 0) {
+                nums[left] = nums[right];
+                nums[right] = 0;
+                left++;
+                right++;
+            }
         }
     }
 }
